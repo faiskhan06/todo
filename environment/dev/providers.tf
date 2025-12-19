be2a856ae64c86a63cecf2a3f32e023c34b68dd3
faiskhan06 <faisalec06@gmail.com>
2025-12-19 21:29:17 +0530

    Update providers.tf

--- environment/dev/providers.tf
+++ environment/dev/providers.tf
@@ -1,10 +1,9 @@
 
 terraform {
-  required_version = ">= 1.3.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.4.0"
+      version = "4.57.0"
     }
   }
 
